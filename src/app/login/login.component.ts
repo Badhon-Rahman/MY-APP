0ea7b77c9fa4ceb7ae7132cc26f3f097a11ed9f4
Badhon <badhon.mostafizur@gmail.com>
2018-09-08 23:36:51 +0600

    allignment

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
